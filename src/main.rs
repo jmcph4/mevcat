21c67769cbd20cd58ba34201307ce805fcb3a2fb
jmcph4 <jack@jmcph4.dev>
2025-01-24 17:35:25 +1000

    `eth_sendBundle`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use ajj::{HandlerCtx, ResponsePayload, Router};
+use ajj::Router;
+use alloy_primitives::FixedBytes;
+use alloy_rpc_types_mev::{EthBundleHash, EthSendBundle};
 
 use clap::Parser;
 use log::info;
@@ -8,41 +10,12 @@ use crate::cli::Opts;
 pub mod cli;
 
 pub fn router() -> Router<()> {
-    Router::<u64>::new()
-        .route(
-            "double",
-            |params: u64| async move { Ok::<_, ()>(params * 2) },
-        )
-        .route("add", |params: u64, state: u64| async move {
-            Ok::<_, ()>(params + state)
+    Router::new().route("eth_sendBundle", |bundle: EthSendBundle| async move {
+        info!("Received bundle: {:?}", bundle);
+        Ok::<EthBundleHash, ()>(EthBundleHash {
+            bundle_hash: FixedBytes::ZERO,
         })
-        // Routes get a ctx, which can be used to send notifications.
-        .route("notify", |ctx: HandlerCtx| async move {
-            if ctx.notifications().is_none() {
-                // This error will appear in the ResponsePayload's `data` field.
-                return Err("notifications are disabled");
-            }
-
-            let req_id = 15u8;
-
-            tokio::task::spawn_blocking(move || {
-                // something expensive goes here
-                let result = 100_000_000;
-                let _ = ctx.notify(&serde_json::json!({
-                  "req_id": req_id,
-                  "result": result,
-                }));
-            });
-            Ok(req_id)
-        })
-        .route("error_example", || async {
-            // This will appear in the ResponsePayload's `message` field.
-            ResponsePayload::<(), ()>::internal_error_message(
-                "this is an error".into(),
-            )
-        })
-        // The router is provided with state, and is now ready to serve requests.
-        .with_state::<()>(3u64)
+    })
 }
 
 #[tokio::main]
