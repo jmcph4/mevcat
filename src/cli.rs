0262ea0ef88d3b3c01689cf73729e335d028b6d0
jmcph4 <jack@jmcph4.dev>
2025-01-24 18:18:37 +1000

    More CLI options

--- src/cli.rs
+++ src/cli.rs
@@ -2,10 +2,10 @@ use std::net::SocketAddr;
 
 use clap::Parser;
 
-const DEFAULT_BIND_SOCK: &str = "0.0.0.0:3000";
-
 #[derive(Clone, Debug, Parser)]
 pub struct Opts {
-    #[clap(long, short, action, default_value=DEFAULT_BIND_SOCK)]
-    pub listen: SocketAddr,
+    #[clap(long, short, action)]
+    pub listen: Option<SocketAddr>,
+    #[clap(long, short)]
+    pub port: Option<u16>,
 }
